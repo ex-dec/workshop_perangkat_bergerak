7f4d476148d57fa94c6b5ce910e22b7bc714d4b8
Krisna Wahyu Setyawan <exclusive.dec4@gmail.com>
2023-03-30 05:03:18 +0700

    init commit percobaan

---
+++ lib/detail_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget> [
+            Image.asset('assets/images/submarine.jpg'),
+            Container(
+              margin: const EdgeInsets.only(top: 15),
+              child: const Text(
+                "Surabaya Submarine Monument",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Lobster'
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget> [
+                  Column(
+                    children: const <Widget> [
+                      Icon(Icons.calendar_today),
+                      Text('Open Everyday'),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 30, right: 30, bottom: 20),
+              child: const Text(
+                "Museum inside a decommisssioned Russion war submarine with tours & an adjacent park with cafes. Clean and well maintained. Car park cost 10k, entrace fee 15k/person. You can see KRI Pasopati there, it is a russian whiskey class. You can also watch the video about the Indonesian Navy at building beside the submarine",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Oxygen'
+                ),
+              ),
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget> [
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Image.asset('assets/images/monkasel_1.jpg'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Image.asset('assets/images/monkasel_2.jpg'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Image.asset('assets/images/monkasel_3.jpg'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:workshop_perangkat_bergerak/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WPPB',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Workshop Flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      title: 'Contacts',
+      theme: ThemeData(),
+      home: const DetailScreen(),
     );
   }
 }
