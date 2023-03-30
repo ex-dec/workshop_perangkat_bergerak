4ec8d5b99910eb805b102030874b5174ef587a5b
Krisna Wahyu Setyawan <exclusive.dec4@gmail.com>
2023-03-30 08:03:29 +0700

    update: init commit percobaan

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:workshop_perangkat_bergerak/detail_screen.dart';
+import 'package:workshop_perangkat_bergerak/model/tourism_place.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -9,17 +11,52 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Wisata Bandung"),
       ),
+      body: ListView.builder(itemBuilder: (context, index){
+        final TourismPlace place = tourismPlaceList[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return DetailScreen(place: place);
+            }));
+          },
+          child: listItem(place),
+        );
+      },
+      itemCount: tourismPlaceList.length,
+      ),
     );
   }
 
-  Widget listItem(){
+  Widget listItem(TourismPlace place){
     return Card(
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget> [
           Expanded(
             flex: 1,
-            child: Image.asset("assets/images/submarine.jpg"),
+            child: Image.asset(place.imageAsset),
+          ),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    place.name,
+                    style: const TextStyle(
+                      fontSize: 16
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(place.location),
+                ],
+              ),
+            ),
           ),
         ],
       ),
