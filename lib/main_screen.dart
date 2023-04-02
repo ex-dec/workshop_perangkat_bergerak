ea82909f04abe07bd125653a36a9dc510b94613a
Krisna Wahyu Setyawan <exclusive.dec4@gmail.com>
2023-04-02 21:04:57 +0700

    initial commit of setstate

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,65 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:workshop_perangkat_bergerak/detail_screen.dart';
+import 'package:workshop_perangkat_bergerak/done_tourism_list.dart';
 import 'package:workshop_perangkat_bergerak/model/tourism_place.dart';
+import 'package:workshop_perangkat_bergerak/tourism_list.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<TourismPlace> doneTourismPlaceList = [];
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: const Text("Wisata Jawa Timur"),
-      ),
-      body: ListView.builder(itemBuilder: (context, index){
-        final TourismPlace place = tourismPlaceList[index];
-        return InkWell(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context){
-              return DetailScreen(place: place);
-            }));
-          },
-          child: listItem(place),
-        );
-      },
-      itemCount: tourismPlaceList.length,
-      ),
-    );
-  }
-
-  Widget listItem(TourismPlace place){
-    return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget> [
-          Expanded(
-            flex: 1,
-            child: Image.asset(place.imageAsset),
-          ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    place.name,
-                    style: const TextStyle(
-                      fontSize: 16
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(place.location),
-                ],
-              ),
-            ),
-          ),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context){
+                  return DoneTourismList(doneTourismPlaceList: doneTourismPlaceList);
+                }
+                )
+              );
+            },
+            icon: const Icon(Icons.done_outline)
+          )
         ],
       ),
+      body: TourismList(doneTourismPlaceList: doneTourismPlaceList,),
     );
   }
 }
